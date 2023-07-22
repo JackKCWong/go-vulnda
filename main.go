d12ea8b040b2c727bb48d977299654f8b26745cd
JackKCWong <jiajue.huang@live.com>
2023-07-22 17:30:08 +0800

    extend timeout to 15s

--- main.go
+++ main.go
@@ -88,7 +88,7 @@ func main() {
 }
 
 func getVulnReport(vc client.Client, modulePath string, updated time.Time) []*osv.Entry {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 
 	fsPath, err := client.EscapeModulePath(modulePath)
